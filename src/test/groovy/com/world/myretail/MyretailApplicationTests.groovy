a0c3d7321e31540ef400cfd1a1e6ae41c769bb7e
lakshmipriya.gopalsamy <lakshmipriya.gopalsamy@bestbuy.com>
2020-02-13 22:09:20 -0600

    Initial commit

---
+++ src/test/groovy/com/world/myretail/MyretailApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.world.myretail
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyretailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
