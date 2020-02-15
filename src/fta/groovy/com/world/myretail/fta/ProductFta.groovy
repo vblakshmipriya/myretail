ef05666f87a39a69cea74c2559aa9ded6b260263
priya <lakshmipriya.gopalsamy@bestbuy.com>
2020-02-15 13:59:41 -0600

    simple get product endpoint.

--- src/fta/groovy/com/world/myretail/fta/ProductFta.groovy
+++ src/fta/groovy/com/world/myretail/fta/ProductFta.groovy
@@ -1,5 +1,6 @@
 package com.world.myretail.fta
 
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
 class ProductFta extends BaseFta {
@@ -9,6 +10,6 @@ class ProductFta extends BaseFta {
     ResponseEntity response = restTemplate.getForEntity('/v1/product/id/1234567', Map)
 
     then:
-    response.statusCode == 200
+    response.statusCode == HttpStatus.OK
   }
 }
